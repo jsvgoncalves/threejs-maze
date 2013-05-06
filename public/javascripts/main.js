e802c0ee47600f51b34c34fef4feba2fe994149d
Vitor Figueira <vitorsemeano@gmail.com>
2013-05-06 14:55:02 +0100

    vários bugs corrigidos, ligação possível com troca de mensagens entre server o cliente

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -6,8 +6,8 @@ function start_connection(){
 
 	var host = $('#host').val(),
 		port = $('#port').val();
-	var link = 	"ws:" + host + ":" + port + "/chat";
-	var socket = io.connect('', {'resource': '/chat'});
+	
+	//var socket = io.connect('', {'resource': '/chat'});
 	 var loginurl = "";
     var pathname = document.location.pathname;
     var lastdot = pathname.lastIndexOf("/");
@@ -15,31 +15,31 @@ function start_connection(){
         loginurl = pathname.substr(1, lastdot);
     }
 
-    alert(loginurl + 'chat');
 
-    var socket = io.connect('ws://127.0.0.1:8080', {'resource':loginurl + 'chat'});
+    var socket = io.connect('ws://' + host + ':' + port, {'resource':loginurl + 'labyrinth'});
 
     socket.on('error', function(reason){
     	alert('error:' + reason);
     });
     
-	
+    socket.on('chat', function(data){
+        alert('resposta recebidac: ' + data);
+    });
+
+
+
+
 	socket.on('connect', function () {
-        /*content.html($('<p>', { text: 'Atmosphere connected using ' + this.socket.transport.name}));
-        input.removeAttr('disabled').focus();
-        status.text('Choose name:');
-
-        $.each(this.socket.transports, function(index, item) {
-            $("#transport").append(new Option(item, item));
-        });*/
-
+      
 		alert('connected');
+
+        socket.emit('chat', 'lol\n');
 		socket.on('disconnect', function () {
-	        console.log('DISCONNESSO!!! ');
-	        //count--;
-	        //io.sockets.emit('count', {
-	          //  number: count
+	        alert('DISCONNESSO!!! ');
+	        
         });
+
+
     });
 
 
